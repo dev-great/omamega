80e528d01609e6e7295517cecad572fb5b5571b2
Marshal Greatness <31458994+dev-great@users.noreply.github.com>
2023-12-19 22:34:32 +0100

    Update forms.py

--- Authorization/forms.py
+++ Authorization/forms.py
@@ -72,21 +72,3 @@ class UserAdminCreationForm(forms.ModelForm):
         if commit:
             user.save()
         return user
-
-
-class UserAdminChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = User
-        fields = ['email', 'password', 'is_active', 'admin']
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
